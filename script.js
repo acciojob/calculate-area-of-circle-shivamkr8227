fb6fd7cb810ed11406e1f4319d935207bf1be8f1
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 14:17:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const radius = parseFloat(prompt("Enter the radius of the circle:"));
+const area = Math.PI * Math.pow(radius, 2);
+const roundedArea = area.toFixed(2);
+
+console.log(`The area of the circle with radius ${radius} is ${roundedArea}`);
